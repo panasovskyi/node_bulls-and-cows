97d037bb2f3f32369f2b93358c7909635baff6a2
panasovskyi <ie.panasovskyi@gmail.com>
2025-09-26 13:23:26 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import readline from 'readline';
-import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
-import { generateRandomNumber } from './modules/generateRandomNumber.js';
-import { getBullsAndCows } from './modules/getBullsAndCows.js';
+const readline = require('readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
 
 const terminal = readline.createInterface({
   input: process.stdin,
