5789a845b5dd58137452ebc98b1358a653071af6
panasovskyi <ie.panasovskyi@gmail.com>
2025-09-26 21:05:34 +0100

    solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -10,7 +10,7 @@ function generateRandomNumber() {
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const firstIndex = Math.floor(Math.random() * 9) + 1;
 
-  const firtstDigit = numbers.splice(firstIndex, 1);
+  const firtstDigit = numbers.splice(firstIndex, 1)[0];
 
   let number = firtstDigit;
 
